efb8789e295a3d7dfa34c702831e95aa9afa9b75
Eugine <eeejerish@gmail.com>
2021-06-27 11:43:03 +0530

    Initial commit

---
+++ src/test/java/com/WeekEndIdeas/WeekEndIdeasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WeekEndIdeas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekEndIdeasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
